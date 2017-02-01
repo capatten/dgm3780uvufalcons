8765841a00096705d53390ac81d0fce0802c870b
Mike Cowley <Mykie92@Mikes-Air-346.ad.uvu.edu>
2017-01-31 19:43:32 -0700

    FB Sharing Dialog

--- index.php
+++ index.php
@@ -5,6 +5,26 @@
 		<meta charset="UTF-8">
 	</head>
 	<body>
+        
+        
+        
+        <!-- facebook Sharing button script -->
+    <div id="fb-root"></div>
+    <script>(function(d, s, id) {
+      var js, fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) return;
+      js = d.createElement(s); js.id = id;
+      js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.8&appId=590347651155605";
+      fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));</script>
+    
+    <!-- Facebook Sharing button -->
+        <div class="fb-share-button" data-href="https://developers.facebook.com/docs/plugins/" data-layout="button_count" data-size="small" data-mobile-iframe="true"><a class="fb-xfbml-parse-ignore" target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fdevelopers.facebook.com%2Fdocs%2Fplugins%2F&amp;src=sdkpreparse">Share</a></div>
+    
+        
+        
+        
+        
 		<div id="fb-root"></div>
 		<script
   src="https://code.jquery.com/jquery-3.1.1.min.js"
